16cec73bd4de8a9d544351ee8fbef2188fcee395
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-02-05 16:26:29 +0200

    Search component implemented, added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import Search from './components/Search'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
 
   return(
-    <>
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="./wtw-logo.png" alt="" />
+            <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          </header>
+        </div>
+      </div>
+    </main>
 
-    </>
+    
   )
-  
+
 }
 
 export default App
