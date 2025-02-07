e8263a82b7207fdebab667cf6f5138d4f63844b2
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2025-02-07 15:29:32 +0200

    new logo, pagination css

--- src/App.jsx
+++ src/App.jsx
@@ -124,7 +124,7 @@ const App = () => {
       <div className="pattern">
         <div className="wrapper">
           <header>
-            <img src="./wtw-logo.png" alt="" />
+            <img src="./wtw-logo-3.png" alt="" />
             <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
             <div className="selectables">
